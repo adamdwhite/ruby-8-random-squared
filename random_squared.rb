8ab3737a5cba205ec1ff249df126e102f344390f
Adam D. White <adamwhite82@gmail.com>
2017-10-10 16:24:56 -0500

    finished exercise

---
+++ random_squared.rb
@@ -0,0 +1,17 @@
+# Instructions
+
+
+# Using the Random class, generate a list of 20 random numbers between 0 and 49.
+random_num = 20.times.map{rand 50}
+
+# With the resulting array, build a new array that contains each number squared. For example, if the original list is [2, 5], the final list will be [4, 25].
+random_num.each{|counter| print counter*counter, " "}
+
+=begin
+eats shoots and leaves 
+here we go
+ruby 
+here we go
+=end 
+
+
